051877facaeeb88795c34e0f5e7969e6948946e5
Nityant17 <nityant17@gmail.com>
2024-12-18 00:53:30 +0530

    Update Code.py
    streamlined the code

--- Code.py
+++ Code.py
@@ -68,53 +68,48 @@ forbidden_indices = {}
 m = 2
 c = 'c'
 while c == 'c':
-    print("Enter colour for each letter by entering g, b or y for green, black or yellow and just press enter if word is not in wordle")
-    l=[]
-    for i in range(0,5):
-        x = input("Enter colour:")
-        l.append(x)
-    # print(l)
-    
-    index_of_g = []
-    index_of_b = []
-    index_of_y = []
-    for i in range(len(l)):
-        if l[i] == 'g': 
-            index_of_g.append(i)
-        if l[i] == 'b': 
-            index_of_b.append(i)
-        if l[i] == 'y': 
-            index_of_y.append(i)
-    # print("green:",index_of_g)
-    # print("black:",index_of_b)
-    # print("yellow:",index_of_y)
-    
-    for i in index_of_g:
-        index_char_conditions.append((i,guess[i]))
-    for i in index_of_b:
-        exclude_letters.append(guess[i])
-    for i in index_of_y:
-        must_contain_letters.append(guess[i])
-        if guess[i] not in forbidden_indices:
-            forbidden_indices[guess[i]] = []
-        forbidden_indices[guess[i]].append(i)
-    index_char_conditions = list(dict.fromkeys(index_char_conditions))
-    must_contain_letters = list(dict.fromkeys(must_contain_letters))
-    # print("Greened:",index_char_conditions)
-    # print("Blacked:",exclude_letters)
-    # print("Yellowed:",must_contain_letters)
-    # print("Index of Yellow:",forbidden_indices)
-    
-    # Find the next word based on all conditions
-    if l == ['', '', '', '', '']:
-        next_word, next_value = word_finder(
-            score,
-            idx_char_pairs=index_char_conditions,
-            exclude_letters=exclude_letters,
-            must_contain_letters=must_contain_letters,
-            forbidden_indices=forbidden_indices,
-            n=m )
-    else:
+    v = input("Did the previous word exist? y or n:")
+    if v == 'y':
+        m = 2
+        print("Enter colour for each letter by entering g, b or y for green, black or yellow")
+        l=[]
+        for i in range(0,5):
+            x = input("Enter colour:")
+            l.append(x)
+        # print(l)
+        
+        index_of_g = []
+        index_of_b = []
+        index_of_y = []
+        for i in range(len(l)):
+            if l[i] == 'g': 
+                index_of_g.append(i)
+            if l[i] == 'b': 
+                index_of_b.append(i)
+            if l[i] == 'y': 
+                index_of_y.append(i)
+        # print("green:",index_of_g)
+        # print("black:",index_of_b)
+        # print("yellow:",index_of_y)
+        
+        for i in index_of_g:
+            index_char_conditions.append((i,guess[i]))
+        for i in index_of_b:
+            exclude_letters.append(guess[i])
+        for i in index_of_y:
+            must_contain_letters.append(guess[i])
+            if guess[i] not in forbidden_indices:
+                forbidden_indices[guess[i]] = []
+            forbidden_indices[guess[i]].append(i)
+        index_char_conditions = list(dict.fromkeys(index_char_conditions))
+        must_contain_letters = list(dict.fromkeys(must_contain_letters))
+        # print("Greened:",index_char_conditions)
+        # print("Blacked:",exclude_letters)
+        # print("Yellowed:",must_contain_letters)
+        # print("Index of Yellow:",forbidden_indices)
+        
+        # Find the next word based on all conditions
+        
         next_word, next_value = word_finder(
             score,
             idx_char_pairs=index_char_conditions,
@@ -122,8 +117,18 @@ while c == 'c':
             must_contain_letters=must_contain_letters,
             forbidden_indices=forbidden_indices,
             n=1 )
-    print(f"Next word: {next_word}, Value: {next_value}")
     
+    elif v == 'n':
+        next_word, next_value = word_finder(
+            score,
+            idx_char_pairs=index_char_conditions,
+            exclude_letters=exclude_letters,
+            must_contain_letters=must_contain_letters,
+            forbidden_indices=forbidden_indices,
+            n=m )
+        m+=1
+    
+    print(f"Next word: {next_word}, Value: {next_value}")
     g = []
     for i in next_word:
         g.append(i)
@@ -132,9 +137,5 @@ while c == 'c':
     c = input("Enter c to continue and end to end:")
     if c == "end":
         break
-    v = input("Did the previous word exist? y or n:")
-    if v == 'n':
-        m+=1
-    elif v == 'y':
-        m = 2
+
 print("Byeee...")
